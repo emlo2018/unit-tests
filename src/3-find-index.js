8aa685a41cffba8ebd5ed5e1e745266aa2666559
Emelie Loheden <emelie_loheden@hotmail.com>
2020-02-29 13:55:18 +0100

    4 angle-calculator and 3 find-index, but I dont understand why 3 did  pass

--- src/3-find-index.js
+++ src/3-find-index.js
@@ -1,3 +1,13 @@
 export const findIndex = (array, value) => {
-  return
-}
+  if (array == null) 
+      return void 0;
+    if (value == null) 
+      return array[0];
+    if (value < 0)
+      return [value];
+    return array.slice(0, value);
+  }
+
+
+
+
--- src/4-angle-calculator.js
+++ src/4-angle-calculator.js
@@ -1,3 +1,14 @@
 export const angleCalculator = (turns) => {
-  return
+  if (turns === 0 ) {
+    return turns * 0
+  }
+  else if (turns === 1 ) {
+    return turns * 360
+  }
+  else if (turns === 1.5 ) {
+    return 360 * 1.5
+
+  } else (turns === 2); {
+    return 360 * 2
+  }
 }
